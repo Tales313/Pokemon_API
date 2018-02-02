796c009700db48cf8052b1c21c69b92c64ea6bf1
Tales Medeiros <t4les313@gmail.com>
2018-02-01 22:15:37 -0300

    algumas modificacoes

--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@
 //link das infos do bulbasauro. Pra mudar o pokemon
 //é só mudar o id no final do link
 
-const pokemons = 'https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0'
+const pokemonsG1 = 'https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0'
 const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/'
+const pkm_click_audio = document.querySelector('#pkm-click-audio')
 let sectionPokemons = document.querySelector('#pokemons')
 let infosEimg = document.querySelector('#infosEimg')
-let icone = document.querySelector('.fa-bars')
-let pkm_click_audio = document.querySelector('#pkm-click-audio')
 let body = document.querySelector('body')
 let is_box_visible = false
 
 // INICIO DAS CHAMADAS DE FUNCOES
 gerarPokemons()
-icone.addEventListener('click', function(){
+$('.fa-bars').click(function(){
   if(!is_box_visible){
     body.style = "overflow: hidden;"
     is_box_visible = true
@@ -25,17 +24,20 @@ icone.addEventListener('click', function(){
 //FIM DAS CHAMADAS DE FUNCOES
 
 function gerarPokemons(){
-  fetch(pokemons).then(res => res.json()).then(function(data){
+  fetch(pokemonsG1).then(r => r.json()).then(function(data){
     let array = []
-    let i = 0
+    let i = 1
     for(let pkm of data['results']){ // do bulbasauro até o mew
       array.push(`<div class="pokemon">
-        <img class="img-pokemon" src="imgs/sprites/default/${i+1}.png" onclick="mudarLightBox(${i+1})">
+        <img class="img-pokemon" data-id="${i}" src="imgs/sprites/default/${i}.png">
         <span>${pkm['name']}</span>
         </div>`)
       i++
     }
-    sectionPokemons.innerHTML = array.join('')
+    sectionPokemons.insertAdjacentHTML('afterbegin', array.join(''))
+    $('.img-pokemon').click(function(){
+      mudarLightBox($(this).attr('data-id'))
+    })
   })
 }
 
@@ -43,17 +45,22 @@ function mudarLightBox(id){
   pkm_click_audio.play()
   fetch(pokemonUrl+id).then(res => res.json())
   .then(function(data){
-    let tipos = data['types'].map(tipo => tipo['type']['name']).join('/')
-    let str = `<div id="infos">
-      <p>Nome: ${data['name']}</p>
-      <p>Id: ${id}</p>
-      <p>Peso: ${data['weight']}</p>
-      <p>Tipo(s): ${tipos}</p>
-    </div>
-    <div id="box-img-div">
-      <img class="box-img" src="imgs/sprites/default/${id}.png">
-      <img class="box-img" src="imgs/sprites/back/${id}.png">
-    </div>`
-    infosEimg.innerHTML = str
+    let str = lightBoxHtml(data, id)
+    infosEimg.innerHTML = ""
+    infosEimg.insertAdjacentHTML('afterbegin', str)
   })
+}
+
+function lightBoxHtml(data, id){
+  let tipos = data['types'].map(tipo => tipo['type']['name']).join('/')
+  return `<div id="infos">
+    <p>Nome: ${data['name']}</p>
+    <p>Id: ${id}</p>
+    <p>Peso: ${data['weight']}</p>
+    <p>Tipo(s): ${tipos}</p>
+  </div>
+  <div id="box-img-div">
+    <img class="box-img" src="imgs/sprites/default/${id}.png">
+    <img class="box-img" src="imgs/sprites/back/${id}.png">
+  </div>`
 }
